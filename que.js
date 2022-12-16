2dd9aaa054b3faf642c33729b6f7227c061b9312
ilknur2603 <ilknurdayanc2603@gmail.com>
2022-12-15 21:22:31 -0700

    que.js added

---
+++ que.js
@@ -0,0 +1,69 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following Attirubute is used to include External JS  code inside your HTML Document ?",
+    answer: "src",
+    options: [
+      "link",
+      "script",
+      "ext",
+      "src",
+    ]
+  },
+    {
+    numb: 2,
+    question: "The ____ attribute is used to specify the charter encoding used in an external script file.",
+    answer: "charset",
+    options: [
+      "charset",
+      "character",
+      "type",
+      "None of the above",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Javascript was invented at ______Lab.",
+    answer: "Netscape",
+    options: [
+      "Google Lab",
+      "Netscape",
+      "AT&T Bell lab",
+      "Sun Microsystem",
+    ]
+  },
+    {
+    numb: 4,
+    question: "Executable single line of Script is called as",
+    answer: "Statement in Javascript",
+    options: [
+      "Breakpoint in JS",
+      "Line in JS",
+      "Statement in Javascript",
+      "None of the above",
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following purpose, Javascript is designed for?",
+    answer: "To add interactivity to HTML Pages",
+    options: [
+      "To Execute Query Related to DB on Server",
+      "To style HTML Pages",
+      "To Perform Server Side Scripting Operation",
+      "To add interactivity to HTML Pages",
+  
+    ]
+  },
+];
+/* question: "Which of the following is an axample of the ternary operator which can be used to shorten if ..else statement?",
+    answer: "(x==2) ? doSomething () :
+    doAnotherThing();",
+    options: [
+      "(x==2) ? doSomething () :
+      doAnotherThing();",
+      "x == 2 && doSomething()",
+      "x == 2 || doSomething();",
+      "if(x ==) { doSomething();"
+*/  
